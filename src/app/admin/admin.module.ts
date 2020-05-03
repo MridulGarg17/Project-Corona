e6652b1e6f326ebd534b2d88233341e7b1345746
MridulGarg17 <mridulgarg17@gmail.com>
2020-05-03 16:24:00 +0530

    login component corrected

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,25 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminComponent } from './admin/admin.component';
-import { LoginComponent } from './login/login.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { MaterialDesignModule } from '../material-design/material-design.module';
 
 
 @NgModule({
-  declarations: [AdminComponent, LoginComponent],
+  declarations: [AdminComponent],
   imports: [
     CommonModule,MaterialDesignModule,
     ReactiveFormsModule,RouterModule.forChild([
       {
         path: '',
         component: AdminComponent
-        
-      },
-      {
-        path: 'login',
-        component: LoginComponent
         
       }
   ]
