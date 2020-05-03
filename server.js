ebfd0c0c7386a56eb8bdbf0d5fc4ebef1f25fa4c
MridulGarg17 <mridulgarg17@gmail.com>
2020-05-03 15:51:55 +0530

    express js included

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+const path=require('path');
+
+app.use(express.static(__dirname+'/dist/Project-Corona'));
+
+app.listen(process.env.PORT || 8080);
+app.get('/*',function(req,res){
+    res.sendFile(path.join(__dirname+'/dist/Project-Corona/index.html'));
+})
+
+console.log('Console listemenfin');
